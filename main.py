e90d980f29946d1b55772762c727e8f8f57e3a59
TarjeC <122256777+pollikk@users.noreply.github.com>
2025-04-01 12:07:16 +0200

    Rotation functionality (#5)
    * Added rotate class
    
    * minor change, cleaned code

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ SCREEN_X = 1024
 SCREEN_Y = 768
 
 PLAYER_SPAWN = 350, 200
-PLAYER_SIZE = (30, 30)
+PLAYER_SIZE = (45, 45)
 
 PLAYER_TWO_IMG = "images/Triangle.png"
 PLAYER_ONE_IMG = "images/Triangle_red.png"
 BACKGROUND_IMG = "images/green_background.png"
+
+STARTING_ANGLE = 0 
 
 pygame.init()
 game_running = True
@@ -19,21 +21,22 @@ screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
 myBackground = pygame.image.load(BACKGROUND_IMG)
 
 if(__name__ == "__main__"):
-
+    playerOne = ship(PLAYER_ONE_IMG, PLAYER_SPAWN, PLAYER_SIZE, STARTING_ANGLE)
+    players = pygame.sprite.Group()
+    players.add(playerOne)
+# -------------------------- GAME LOOP -------------------------- #    
     while game_running:
         screen.blit(myBackground, (0, 0))
-
-        # ------- RECIPE FOR MAKING A OBJECT, GROUPING IT, AND DRAWING TO SCREEN ----------- #
-        playerOne = ship(PLAYER_ONE_IMG, PLAYER_SPAWN, PLAYER_SIZE)
-        players = pygame.sprite.Group()
-        players.add(playerOne)
         players.draw(screen)
-        # ------- END OF RECIPE ----------- #
-
         pygame.display.update()
 
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    print("Left arrow key pressed!")
+                    playerOne.rotate(-1)
             if event.type == pygame.QUIT:
+
                 game_running = False
 
 pygame.quit()
