0a633fd89583241149a7c72bf46e338b24bacc92
TarjeC <122256777+pollikk@users.noreply.github.com>
2025-04-01 12:48:30 +0200

    Player controlls (#7)
    * Testing conflicts
    
    * Testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from testing_object import ship
+from player_controller import controller
 import pygame
 from pygame import Vector2
 
@@ -26,15 +27,29 @@ if(__name__ == "__main__"):
     players.add(playerOne)
 # -------------------------- GAME LOOP -------------------------- #    
     while game_running:
+
+
+
+
+
+
+
+
+
+
+
+        
         screen.blit(myBackground, (0, 0))
         players.draw(screen)
         pygame.display.update()
 
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    print("Left arrow key pressed!")
-                    playerOne.rotate(-1)
+            controller.update(event)
+
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_LEFT:
+            #         print("Left arrow key pressed!")
+            #         playerOne.rotate(-1)
             if event.type == pygame.QUIT:
 
                 game_running = False
