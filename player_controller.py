bbf557878eb25b8f67b3ecb3b660a98c916fa5f2
TarjeC <122256777+pollikk@users.noreply.github.com>
2025-04-10 13:14:08 +0200

    Thrust boolean (#19)
    * added boolean for thrust for playerone
    
    * added boolean for thrust

--- player_controller.py
+++ player_controller.py
@@ -3,13 +3,14 @@ import pygame
 class controller():
     def update(keys, left, right, up, down):
         rotation = 0
+        thrust = False
         if keys[left]:
             rotation = 2
         elif keys[right]:
             rotation = -2
         if keys[up]:
-            print("test")
+            thrust = True
         if keys[down]:
             print("test")
 
-        return rotation
+        return rotation, thrust
