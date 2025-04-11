623e22cdccfffc7483573d9065f279ddfa0f889c
Tarje C <ttc_96@hotmail.no>
2025-04-11 11:37:14 +0200

    General fixes and cleanup. added dt to player.update

--- config.py
+++ config.py
@@ -1,20 +1,27 @@
+# ----------- SCREEN CONFIGS ----------- 
 SCREEN_X = 1024
 SCREEN_Y = 768
 
+
+# ----------- PLAYER CONFIGS ----------- 
 PLAYER_SIZE = (75, 75)
 PLAYER_ONE_POSITION = (350, 200)
 PLAYER_TWO_POSITION = (800, 200)
+ROTATION_SPEED = 50
 
-GRAVITATION = 0.3
-STARTING_ANGLE = 0
-
-SPEED = 0.8
+# ----------- GAME CONFIGS ----------- 
+GRAVITATION = 100
 TARGET_FPS = 60
 
+# ----------- SHIP CONFIGS ----------- 
+SPEED = 300
+STARTING_ANGLE = 0
+FIRE_RATE = 0.5
+
+# ----------- OBSTACLE CONFIGS ----------- 
 # Obstacle cordinates (x,y)
 obstacleOne_X_Y= 200, 50
 obstacleTwo_X_Y= 600, 300
-
 
 #Obstacle colors (red, green, blue)
 obstacleOne_rgb = 150, 250, 30
